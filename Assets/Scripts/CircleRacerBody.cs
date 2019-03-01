3b59377b7d4b8525d4b6f85b7f0be5ce868d0601
Autofire <autofire555@gmail.com>
2019-02-28 20:41:48 -0800

    Got a basic circle prototype working.

--- Assets/Scripts/CircleRacerBody.cs
+++ Assets/Scripts/CircleRacerBody.cs
@@ -2,23 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RacerBody : MonoBehaviour {
+public class CircleRacerBody : MonoBehaviour {
 
 	public float velocity = 0f;
 	public float acceleration = 1f;
+	public float dragCoefficient = 0.5f;
 
-	void Start () {
-		
+	public float verticalMoveSpeed = 1f;
+	public float verticalClamp = 2f;
+
+	public void Turn(float magnitude) {
+
+		float clampedMagnitude = Mathf.Clamp(magnitude, -1f, 1f);
+		float rawYPos = transform.position.y + clampedMagnitude * verticalMoveSpeed * Time.deltaTime;
+		float clampedYPos = Mathf.Clamp(rawYPos, -verticalClamp, verticalClamp);
+
+		transform.position = new Vector3(
+			transform.position.x,
+			clampedYPos,
+			transform.position.z
+		);
 	}
-	
+
 	void Update () {
+		MoveForward();
+	}
+
+	private void MoveForward() {
 		// The equation is:
 		//  v = v0 + a * dt
 		//
 		// where v0 is the intial velocity, a is the acceleration, and
 		// dt is the change in time. The result is the new velocity on this
 		// step.
-		velocity += acceleration * Time.deltaTime;
+		velocity =
+			velocity
+			+ acceleration * Time.deltaTime
+			- dragCoefficient * velocity * velocity * Time.deltaTime;
 
 		// The distance to travel in this timestep.
 		float dist = velocity * Time.time;
@@ -35,7 +55,8 @@ public class RacerBody : MonoBehaviour {
 		//
 		// In this case, the total distance traveled is arcLength, which
 		// we have already found. Radius can come out of CircleTrackController.
-		float rotationAmount = dist / CircleTrackController.Instance.radius;
+		float radius = Vector3.Distance(CircleTrackController.Instance.transform.position, transform.position);
+		float rotationAmount = dist / radius;
 
 		// Alright, finally, we can move ourself. 
 		transform.RotateAround(
@@ -43,7 +64,6 @@ public class RacerBody : MonoBehaviour {
 			CircleTrackController.Instance.transform.up,
 			rotationAmount
 		);
-
 	}
 
 }
---
+++ Assets/Scripts/HumanRacerBrain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanRacerBrain : MonoBehaviour {
+
+	public CircleRacerBody body;
+	public string turnAxisName = "Vertical";
+
+	void Update () {
+		body.Turn(Input.GetAxis(turnAxisName));
+	}
+}
