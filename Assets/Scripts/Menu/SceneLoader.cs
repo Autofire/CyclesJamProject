5967d5022654d6a8cc29a49cb3998a33584a40ec
Autofire <autofire555@gmail.com>
2019-03-02 10:27:17 -0800

    Created main menu based on Brackey's tutorial.

---
+++ Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	[SerializeField] private string targetScene;
+	public void loadScene() {
+		// TODO Make this load async first
+		SceneManager.LoadScene(targetScene);
+	}
+}
