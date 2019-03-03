dac780c0378c0c9596e8f3cb93e7748d2e62b26b
Autofire <autofire555@gmail.com>
2019-03-02 19:24:48 -0800

    Added timer.

---
+++ Assets/Scripts/UI/TimerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using ReachBeyond.VariableObjects;
+
+public class TimerController : MonoBehaviour
+{
+	[SerializeField] private TMPro.TextMeshProUGUI textObj;
+	[SerializeField] private FloatConstReference startTime;
+
+	private void Awake() {
+		Assert.IsNotNull(textObj);
+	}
+
+
+	private void OnGUI() {
+		float currentTime = Time.time - startTime.ConstValue;
+
+		int minutes = ((int)currentTime) / 60;
+		int seconds = ((int)currentTime) % 60;
+		float secondFraction = currentTime % 1f;
+		int milliseconds = Mathf.RoundToInt(secondFraction * 1000f);
+
+		string[] timerValues = new string[] { 
+			minutes.ToString("D2"), seconds.ToString("D2"), milliseconds.ToString("D3")
+		};
+
+		textObj.SetText(string.Join( ":", timerValues ));
+	}
+
+}
