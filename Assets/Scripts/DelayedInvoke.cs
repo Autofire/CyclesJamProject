4423aa86cf3a82531dd92049de8cf6adba1c93f7
Autofire <autofire555@gmail.com>
2019-03-02 17:54:23 -0800

    Created countdown at the beginning of the race.

---
+++ Assets/Scripts/DelayedInvoke.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ReachBeyond.EventObjects;
+
+public class DelayedInvoke : MonoBehaviour {
+	[SerializeField] private float delay;
+	[SerializeField] private EventObjectInvoker invoker;
+
+	private float invokeTime;
+
+	private void OnEnable() {
+		invokeTime = Time.time + delay;
+	}
+
+	void Update() {
+		// We use a loop for this in the case that tickFrequency
+		// ends up being shorter than the Update clock. That way, we'll
+		// still count the same way regardless of the time step.
+		if(Time.time >= invokeTime) {
+			invoker.Invoke();
+			enabled = false;
+		}
+	}
+}
+
---
+++ Assets/Scripts/UI/CountdownController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using ReachBeyond.EventObjects;
+using ReachBeyond.VariableObjects;
+
+public class CountdownController : AbstractEventObjectListener
+{
+	[SerializeField] private IntConstReference countdownValue;
+
+	[Tooltip("Sets each of these active, one at a time, each time the value is set to the matching index.")]
+	[SerializeField] private GameObject[] countdownObjects;
+
+	public override void OnRaiseEvent() {
+		for(int i = 0; i < countdownObjects.Length; i++) {
+			countdownObjects[i].SetActive(i == countdownValue.ConstValue);
+		}
+	}
+
+}
