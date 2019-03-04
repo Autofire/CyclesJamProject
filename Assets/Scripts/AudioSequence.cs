92394bcffc3d2f7c204ad2e7b1c13ce97a680a72
Autofire <autofire555@gmail.com>
2019-03-03 20:10:51 -0800

    Created AudioSequencer. Also made looping better.

---
+++ Assets/Scripts/AudioSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioSequence : AbstractApplier
+{
+	public AudioSource source1;
+	public AudioSource source2;
+
+
+	override public void Apply() {
+		source1.Play();
+		source2.PlayDelayed(source1.clip.length);
+	}
+
+	public void Stop() {
+		source1.Stop();
+		source2.Stop();
+	}
+
+}
