a5bc82c521178d983eeeee3302b4d1cb1406140c
Codey Huntting <creativecodey@gmail.com>
2019-03-03 18:34:54 -0800

    Added boost particle effect, imported lots of audio

---
+++ Assets/Scripts/Effects/ParticleEffectOnTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/*
+ * CLASS ParticleEffectOnTrigger
+ * -----------------------------
+ * Instantiates a copy of the given object at the script object's transform
+ * position as soon as it hits a trigger with one of the designated tags,
+ * then destroys the copy after some time
+ * -----------------------------
+ */
+ 
+public class ParticleEffectOnTrigger : MonoBehaviour
+{
+    // Local GameObject enabled/disabled to create the effect
+    [SerializeField] private GameObject particle;
+    // Time for which the particle stays before being destroyed
+    [SerializeField] private float lifetime;
+    // Any trigger with one of these tags will trigger the particle effect
+    [SerializeField] private List<string> triggerTags;
+
+    // If the other collider's tag is in the list, trigger the particle effect
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bool triggerEffect = triggerTags.Contains(collision.tag);
+        if(triggerEffect)
+        {
+            TriggerParticleEffect();
+        }
+    }
+    // Reactivate the local object, then disable it after specified time
+    private void TriggerParticleEffect()
+    {
+        // Reactivate the particle
+        particle.SetActive(false);
+        particle.SetActive(true);
+        // Cause the particle to disable after specified time
+        CancelInvoke();
+        Invoke("DisableParticle", lifetime);
+    }
+    private void DisableParticle()
+    {
+        particle.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Effects/RandomizedAudioSource.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/*
+ * CLASS PlayRandomEffect
+ * ----------------------
+ * Much like an audio source, this script allows a set of effects
+ * to be played at random either on a custom audio source or 
+ * on an audio source on the sound player
+ * ----------------------
+ */ 
+
+public class RandomizedAudioSource : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> clips;  // List of clips that are randomly played
+    [SerializeField]
+    private bool playOnAwake;   // If true, plays a random effect when enabled
+    [SerializeField]
+    private AudioSource source; // Source local to this object. If null, plays on a sound player local source
+
+    private void OnEnable()
+    {
+        if(playOnAwake)
+        {
+            PlayRandomEffect();
+        }
+    }
+
+    public void PlayRandomEffect()
+    {
+        int selection = Random.Range(0, clips.Count);
+        source.clip = clips[selection];
+        source.Play();
+    }
+}
